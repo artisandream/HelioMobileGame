980bb265d5292fd77990a3173be681fe85a2c93f
Unknown <royalterravore@gmail.com>
2017-03-28 21:05:36 -0600

    Added Player Script
    Added HP

---
+++ FinalGame/Assets/Player.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	int hp;
+
+	// Use this for initialization
+	void Start () {
+		hp = 10;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
