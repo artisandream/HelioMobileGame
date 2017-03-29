59d80e679a012e32a257e2429ffff4b0a4917948
Isaac <ikingst7@gmail.com>
2017-03-28 21:07:10 -0600

    Change Player script

---
+++ FinalGame/Assets/Player.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public bool playing = false;
+
+	// Use this for initialization
+	void Start () {
+		playing = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
